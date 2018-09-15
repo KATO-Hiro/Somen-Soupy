a0a528b0a6b5f71d274771f7c92dfdbaa8f82017
Kato Hiroki <k.hiro1818@gmail.com>
2018-09-15 15:05:36 +0900

    Add is_prime() and unit test.

---
+++ tests/test_prime.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from snippets.prime import is_prime
+
+
+class TestPrime(object):
+
+    @pytest.mark.parametrize(('number', 'expected'), [
+        (-1.1, False),
+        (-1, False),
+        (0, False),
+        (1, False),
+        (2, True),
+        (3, True),
+        (3.0, False),
+        (3.1, False),
+        (4, False),
+        (5, True),
+        (11, True),
+        (10 ** 9 + 7, True),
+    ])
+    def test_is_prime(self, number, expected):
+        actual = is_prime(number)
+        assert actual == expected
