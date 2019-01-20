20c4f3232c9ba68bf133cfcbfb0e04f80398e4a0
Kato Hiroki <k.hiro1818@gmail.com>
2019-01-20 15:22:50 +0900

    Add a method in lcm.
    Reason:
    LCM is not in math module.

---
+++ snippets/math/lcm.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''Snippets for lcm.
+
+Available functions:
+- lcm: Compute least common multiple of a and b.
+'''
+
+
+def lcm(a: int, b: int) -> int:
+    '''Compute least common multiple of a and b.
+
+    Args:
+        a: Int of number (greater than 0).
+        b: Int of number (greater than 0).
+
+    Returns:
+        least common multiple.
+
+    Landau notation: O(log n)
+
+    See:
+    https://gist.github.com/endolith/114336/eff2dc13535f139d0d6a2db68597fad2826b53c3
+    '''
+
+    from sys import version_info
+
+    if version_info.major == 3 and version_info.minor >= 5:
+        from math import gcd
+    else:
+        from fractions import gcd
+
+    return a * b // gcd(a, b)
---
+++ tests/test_lcm.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from snippets.math.lcm import lcm
+
+
+class TestFactorization(object):
+
+    @pytest.mark.parametrize(('a', 'b', 'expected'), [
+        (1, 1, 1),
+        (2, 3, 6),
+        (12, 4, 12),
+        (4, 12, 12),
+    ])
+    def test_lcm(self, a, b, expected):
+        actual = lcm(a, b)
+        assert actual == expected
