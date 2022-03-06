6ae4d76a9aab3e9142b04f248ecbddc5f91e8602
Kato Hiroki <k.hiro1818@gmail.com>
2022-03-06 15:41:21 +0900

    :books: Add usage and update description (#179)

--- snippets/string/popcount.py
+++ snippets/string/popcount.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+Usage:
+
+    n = int(input())
+    count = popcount(n)
+"""
+
 
 def popcount(n: int) -> int:
     '''Count set bits in an integer.
 
     Args:
-        n: Int of number (greater than 0).
+        n: Int of number (greater than or equal to 0).
 
     Returns:
         The number of set bits.
