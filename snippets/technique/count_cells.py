8f44e3392c7cf26c6457834e5a91e09104fcdd75
Kato Hiroki <k.hiro1818@gmail.com>
2022-05-05 09:29:45 +0900

    :recycle: Use arg (#209)

--- snippets/technique/count_cells.py
+++ snippets/technique/count_cells.py
@@ -4,10 +4,11 @@
 # See:
 # https://atcoder.jp/contests/hhkb2020/submissions/17301581
 def count_cells(grids, h: int, w: int):
-    up = [[1 if grids[i][j] == '.' else 0 for j in range(w)] for i in range(h)]
-    down = [[1 if grids[i][j] == '.' else 0 for j in range(w)] for i in range(h)]
-    left = [[1 if grids[i][j] == '.' else 0 for j in range(w)] for i in range(h)]
-    right = [[1 if grids[i][j] == '.' else 0 for j in range(w)] for i in range(h)]
+    dot = '.'
+    up = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    down = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    left = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
+    right = [[1 if grids[i][j] == dot else 0 for j in range(w)] for i in range(h)]
     
     # Comment out the following lines if needs.
     # dot_count = 0
@@ -19,29 +20,29 @@ def count_cells(grids, h: int, w: int):
 
     for i in range(h - 2, -1, -1):
         for j in range(w):
-            if (grids[i][j] == '.'):
+            if (grids[i][j] == dot):
                 up[i][j] = up[i + 1][j] + 1
 
     for i in range(1, h):
         for j in range(w):
-            if (grids[i][j] == '.'):
+            if (grids[i][j] == dot):
                 down[i][j] = down[i - 1][j] + 1
 
     for i in range(h):
         for j in range(w - 2, -1, -1):
-            if (grids[i][j] == '.'):
+            if (grids[i][j] == dot):
                 left[i][j] = left[i][j + 1] + 1
 
     for i in range(h):
         for j in range(1, w):
-            if (grids[i][j] == '.'):
+            if (grids[i][j] == dot):
                 right[i][j] = right[i][j - 1] + 1
 
     ans = 0
 
     for i in range(h):
         for j in range(w):
-            if grids[i][j] == '.':
+            if grids[i][j] == dot:
                 count = (up[i][j] + down[i][j] - 1) + (left[i][j] + right[i][j] - 1) - 1
 
                 # Update ans using count.
