7faa2193fabd2a27440c5573f685b6301c78b2d3
Kato Hiroki <k.hiro1818@gmail.com>
2023-04-08 17:41:34 +0900

    :sparkles: Add topological sorting (#246)

---
+++ snippets/graph/topological_sorting.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+"""
+Usage:
+
+    n, m = map(int, input().split())
+    ts = TopologicalSorting(n)
+
+    for _ in range(m):
+        # vertex ai -> bi.
+        ai, bi = map(int, input().split())
+        ai -= 1
+        bi -= 1
+
+        ts.add_edge(frm=ai, to=bi)
+
+    is_DAG, orders = ts.sort()
+"""
+
+from collections import deque
+from typing import List, Tuple
+
+
+class TopologicalSorting:
+    """
+    See:
+    https://atcoder.jp/contests/abc291/submissions/39241055
+    """
+
+    def __init__(self, vertex_count: int) -> None:
+        self.vertex_count = vertex_count
+        self.graph = [[] for _ in range(vertex_count)]
+        self.indegrees = [0] * vertex_count
+
+    def add_edge(self, frm: int, to: int) -> None:
+        '''
+        Args:
+            frm(from) -> to: Vertex number (0-indexed).
+        '''
+        assert 0 <= frm < self.vertex_count
+        assert 0 <= to < self.vertex_count
+
+        self.graph[frm].append(to)
+        self.indegrees[to] += 1
+
+    def sort(self) -> Tuple[bool, List[int]]:
+        '''
+        Returns:
+            is_DAG: Is it DAG (Directed Acyclic Graph) ?
+            orders: Order of vertices (0-indexed).
+        '''
+        que = deque([i for i in range(self.vertex_count) if self.indegrees[i] == 0])
+        results = list()
+
+        if len(que) == 0:
+            return False, []
+
+        while que:
+            if len(que) >= 2:
+                return False, []
+
+            vertex = que.popleft()
+            results.append(vertex)
+
+            for to in self.graph[vertex]:
+                self.indegrees[to] -= 1
+
+                if self.indegrees[to] == 0:
+                    que.append(to)
+
+        if len(results) == self.vertex_count:
+            return True, results
+        else:
+            return False, []
