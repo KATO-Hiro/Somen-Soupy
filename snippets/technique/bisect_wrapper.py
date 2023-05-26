107b752c6b6bfdab99ee8161b6f668e5e2bd8f60
Kato Hiroki <k.hiro1818@gmail.com>
2023-05-26 19:16:17 +0900

    ✨ Add bisect wrapper (#290)

---
+++ snippets/technique/bisect_wrapper.py
@@ -0,0 +1,46 @@
+from bisect import bisect_left, bisect_right
+from typing import List
+
+
+def bisect_lt(sorted_array: List[int], value: int) -> tuple[int | None, int | None]:
+    """Find the largest element < x and its index, or None if it doesn't exist."""
+
+    if sorted_array[0] < value:
+        index: int = bisect_left(sorted_array, value) - 1
+
+        return index, sorted_array[index]
+
+    return None, None
+
+
+def bisect_le(sorted_array: List[int], value: int) -> tuple[int | None, int | None]:
+    """Find the largest element <= x and its index, or None if it doesn't exist."""
+
+    if sorted_array[0] <= value:
+        index: int = bisect_right(sorted_array, value) - 1
+
+        return index, sorted_array[index]
+
+    return None, None
+
+
+def bisect_gt(sorted_array: List[int], value: int) -> tuple[int | None, int | None]:
+    """Find the smallest element > x and its index, or None if it doesn't exist."""
+
+    if sorted_array[-1] > value:
+        index: int = bisect_right(sorted_array, value)
+
+        return index, sorted_array[index]
+
+    return None, None
+
+
+def bisect_ge(sorted_array: List[int], value: int) -> tuple[int | None, int | None]:
+    """Find the smallest element >= x and its index, or None if it doesn't exist."""
+
+    if sorted_array[-1] >= value:
+        index: int = bisect_left(sorted_array, value)
+
+        return index, sorted_array[index]
+
+    return None, None
