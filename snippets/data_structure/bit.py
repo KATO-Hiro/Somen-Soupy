ccb92f7f004218527ffb8a5e5122e61c8eb31a5c
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-23 23:15:25 +0900

    ✨ Update BIT (#303)

--- snippets/data_structure/bit.py
+++ snippets/data_structure/bit.py
@@ -4,68 +4,80 @@
 # Usage:
 # n = int(input())
 # bit = BIT(n)
-# bit.add(index, value)
-# bit.sum(index)
-#
+# bit.add(index, value)  # 0-indexed
+# bit.range_sum(left, right)  # [left, right)
+# bit.lower_bound(value) # = le
 #
 # a = [3, 10, 1, 8, 5]
 # inversion_number = calc_inversion_number(a)
 # print(inversion_number)  # 5
 
-from typing import List
+from typing import Any, List
 
 
 class BIT:
     """Binary Indexed Tree (Fenwick Tree)
 
-    Args:
-        size: List size (greater than 0).
-
+    See:
+    https://atcoder.jp/contests/tessoku-book/submissions/34912434
     """
 
-    def __init__(self, size: int):
+    def __init__(self, size: int) -> None:
         self.size = size
-        self._bit = [0 for _ in range(self.size + 1)]
+        self.size0 = 1 << (size.bit_length() - 1)
+        self.tree = [0] * (size + 1)
 
-    def add(self, index: int, value: int) -> None:
-        """Adds value to list with index i.
+    def add(self, index: int, value: Any) -> None:
+        assert 0 <= index < self.size
 
-        Args:
-            index: The number of index (1-indexed).
-            value: A value
-
-        Landau notation: O(log n)
-        """
+        index += 1
 
         while index <= self.size:
-            self._bit[index] += value
+            self.tree[index] += value
+            # self.tree[index] %= mod
             index += index & -index
 
-    def sum(self, index) -> int:
-        """Calculate the sum of elements from 1 to index.
+    def get(self, index: int) -> Any:
+        return self.sum(index) - self.sum(index - 1)
 
-        Args:
-            index: The number of index (1-indexed).
+    def range_sum(self, left: int, right: int) -> Any:
+        assert 0 <= left <= right <= self.size
 
-        Returns:
-            The sum of elements from 1 to index.
+        return self.sum(right - 1) - self.sum(left - 1)
 
-        Landau notation: O(log n)
-        """
-
+    def sum(self, index: int) -> Any:
+        index += 1
         summed = 0
 
+        assert 0 <= index <= self.size
+
         while index > 0:
-            summed += self._bit[index]
+            summed += self.tree[index]
             index -= index & -index
+            # summed %= mod
 
         return summed
+
+    def lower_bound(self, value: Any) -> int:
+        pos = 0
+        plus = self.size0
+
+        while plus > 0:
+            if pos + plus <= self.size and self.tree[pos + plus] < value:
+                value -= self.tree[pos + plus]
+                pos += plus
+
+            plus //= 2
+
+        return pos
 
 
 # See:
 # https://ikatakos.com/pot/programming_algorithm/dynamic_programming/inversion
 def calc_inversion_number(array: List[int]) -> int:
-    compressed_dict = {element: index for index, element in enumerate(sorted(set(array)), 1)}
+    compressed_dict = {
+        element: index for index, element in enumerate(sorted(set(array)))
+    }
     compressed_list = [compressed_dict[ai] for ai in array]
 
     size = len(compressed_list)
--- tests/test_bit.py
+++ tests/test_bit.py
@@ -1,19 +1,188 @@
 # -*- coding: utf-8 -*-
 
-from typing import List
+from typing import List, Tuple
+
 import pytest
 
-from snippets.data_structure.bit import calc_inversion_number
+from snippets.data_structure.bit import BIT, calc_inversion_number
+
+
+@pytest.fixture
+def bit() -> BIT:
+    return BIT(8)
+
+
+@pytest.fixture
+def a() -> List[int]:
+    return [3, 1, 4, 1, 5, 9, 2, 6]
 
 
 class TestBIT:
+    def test_initial_status(self, bit: BIT) -> None:
+        assert bit.tree == [0] * (8 + 1)  # 0-indexed
 
-    @pytest.mark.parametrize(('a', 'expected'), [
-        ([3, 10, 1, 8, 5], 5),
-        ([1, 2, 3, 4, 5], 0),
-        ([1, 2, 3, 3, 5, 5], 0),
-        ([-1, -1, 2, 3, 3, 5, 5], 0),
-    ])
-    def test_calc_inversion_number(self, a: List[int], expected: int):
+        pair_of_positions = self._generate_pair_of_positions()
+
+        for left, right in pair_of_positions:
+            assert bit.range_sum(left, right) == 0
+
+    def _generate_pair_of_positions(self) -> List[Tuple[int, ...]]:
+        from itertools import combinations_with_replacement
+
+        return list(combinations_with_replacement(range(8 + 1), 2))
+
+    def test_add(self, bit: BIT) -> None:
+        bit.add(0, 1)
+        assert bit.tree == [0, 1, 1, 0, 1, 0, 0, 0, 1]
+
+    def test_add_multiple_times(self, bit: BIT) -> None:
+        expected = [
+            [0, 1, 1, 0, 1, 0, 0, 0, 1],
+            [0, 1, 3, 0, 3, 0, 0, 0, 3],
+            [0, 1, 3, 3, 6, 0, 0, 0, 6],
+            [0, 1, 3, 3, 10, 0, 0, 0, 10],
+            [0, 1, 3, 3, 10, 5, 5, 0, 15],
+            [0, 1, 3, 3, 10, 5, 11, 0, 21],
+            [0, 1, 3, 3, 10, 5, 11, 7, 28],
+            [0, 1, 3, 3, 10, 5, 11, 7, 36],
+        ]
+
+        for i in range(8):
+            bit.add(i, i + 1)
+            assert bit.tree == expected[i]
+
+    def test_add_when_negative_value_is_given(self, bit: BIT) -> None:
+        for i in range(8):
+            bit.add(i, i + 1)
+
+        assert bit.tree == [0, 1, 3, 3, 10, 5, 11, 7, 36]
+
+        bit.add(0, -5)
+        assert bit.tree == [0, -4, -2, 3, 5, 5, 11, 7, 31]
+
+    def test_add_when_zero_is_given(self, bit: BIT) -> None:
+        for i in range(8):
+            bit.add(i, i + 1)
+
+        assert bit.tree == [0, 1, 3, 3, 10, 5, 11, 7, 36]
+
+        bit.add(0, 0)
+        assert bit.tree == [0, 1, 3, 3, 10, 5, 11, 7, 36]
+
+    def test_add_when_positive_floating_point_value_is_given(self, bit: BIT) -> None:
+        bit.add(0, 1.5)
+        assert bit.tree == [0, 1.5, 1.5, 0, 1.5, 0, 0, 0, 1.5]
+
+    def test_add_when_negative_floating_point_value_is_given(self, bit: BIT) -> None:
+        bit.add(0, -1.5)
+        assert bit.tree == [0, -1.5, -1.5, 0, -1.5, 0, 0, 0, -1.5]
+
+    def test_get(self, bit: BIT, a: List[int]) -> None:
+        for index, ai in enumerate(a):
+            bit.add(index, ai)
+
+        for index, ai in enumerate(a):
+            assert bit.get(index) == ai
+
+    @pytest.mark.parametrize(
+        ("left", "right", "expected"),
+        [
+            (0, 8, 36),
+            (0, 7, 28),
+            (1, 7, 27),
+            (1, 3, 5),
+            (2, 2, 0),
+        ],
+    )
+    def test_range_sum(self, bit: BIT, left: int, right: int, expected: int) -> None:
+        for i in range(8):
+            bit.add(i, i + 1)
+
+        assert bit.range_sum(left, right) == expected
+
+    @pytest.mark.parametrize(
+        ("left", "right", "expected"),
+        [
+            (0, 8, 38),
+            (0, 7, 30),
+            (0, 2, 3),
+            (0, 1, 1),
+            (1, 4, 11),
+            (1, 3, 7),
+            (2, 2, 0),
+        ],
+    )
+    def test_range_sum_when_additional_element_is_given(
+        self, bit: BIT, left: int, right: int, expected: int
+    ) -> None:
+        for i in range(8):
+            bit.add(i, i + 1)
+
+        assert bit.range_sum(0, 8) == 36
+
+        bit.add(2, 2)
+        bit.tree = [0, 1, 5, 3, 12, 5, 11, 7, 38]
+
+    @pytest.mark.parametrize(
+        ("value", "expected"),
+        [
+            (0, 0),
+            (1, 0),
+            (2, 1),
+            (3, 1),
+            (4, 2),
+            (6, 2),
+            (36, 7),
+            (37, 8),
+            (38, 8),
+        ],
+    )
+    def test_lower_bound(self, bit: BIT, value: int, expected: int) -> None:
+        for i in range(8):
+            bit.add(i, i + 1)
+
+        assert bit.lower_bound(value) == expected
+
+    @pytest.mark.parametrize(
+        ("index"),
+        [
+            -2,
+            -1,
+            8,
+            9,
+        ],
+    )
+    def test_add_failed_if_invalid_input_is_given(self, bit: BIT, index: int) -> None:
+        with pytest.raises(AssertionError):
+            bit.add(index, None)
+
+    @pytest.mark.parametrize(
+        ("left", "right"),
+        [
+            (-2, 8),
+            (-1, 8),
+            (0, 9),
+            (0, 10),
+            (3, 2),
+            (-1, 8),
+            (-1, 9),
+        ],
+    )
+    def test_range_sum_failed_if_invalid_input_is_given(
+        self, bit: BIT, left: int, right: int
+    ) -> None:
+        with pytest.raises(AssertionError):
+            bit.range_sum(left, right)
+
+    @pytest.mark.parametrize(
+        ("a", "expected"),
+        [
+            ([3, 10, 1, 8, 5], 5),
+            ([1, 2, 3, 4, 5], 0),
+            ([1, 2, 3, 3, 5, 5], 0),
+            ([-1, -1, 2, 3, 3, 5, 5], 0),
+        ],
+    )
+    def test_calc_inversion_number(self, a: List[int], expected: int) -> None:
         actual = calc_inversion_number(a)
         assert actual == expected
