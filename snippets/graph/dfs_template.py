a00e85e376c6dbb8d50ec546fbf654f6f81c0f31
Kato Hiroki <k.hiro1818@gmail.com>
2023-06-26 17:27:32 +0900

    :bug: Fix syntax errors (#305)

--- snippets/graph/dfs_template.py
+++ snippets/graph/dfs_template.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+def main():
+    import sys
     from functools import lru_cache
 
     sys.setrecursionlimit(10**8)
@@ -19,12 +21,12 @@
     @lru_cache(maxsize=None)
     def dfs(cur, parent=-1):
         # Base case
-        if codition:
-            return initial_value # TODO: Update value.
+        # if codition:
+        #     return initial_value  # TODO: Update value.
 
         # Rec case
-        value_min = 10 ** 18 # TODO: Initialize value.
-        value_max = 0 # TODO: Initialize value.
+        value_min = 10**18  # TODO: Initialize value.
+        value_max = 0  # TODO: Initialize value.
 
         for to in graph[cur]:
             if to == parent:
@@ -38,8 +40,10 @@
             # postorder
             # TODO: Update value.
 
-        return value # TODO: Update value.
+        return  # value  # TODO: Update value.
 
     print(dfs(0))
 
 
+if __name__ == "__main__":
+    main()
