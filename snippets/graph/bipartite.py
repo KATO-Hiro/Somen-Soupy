5ab2329c68f185705f05e21a1242b7d24940ed2b
Kato Hiroki <k.hiro1818@gmail.com>
2023-08-04 22:42:13 +0900

    :bug: Enable to count white / black for multiple connected components (#264)

--- snippets/graph/bipartite.py
+++ snippets/graph/bipartite.py
@@ -13,10 +13,18 @@
 #     graph[ai].append(bi)
 #     graph[bi].append(ai)
 
-# results = is_bipartite(start_id=0, graph, colors) 
+# for i in range(n):
+#     if colors[i] != no_color:
+#         continue
 
+#     flag, black_count, white_count = is_bipartite(start_id=i, graph=graph, colors=colors)
 
-from typing import List
+#     if flag:
+#         print(black_count, white_count)
+#     else:
+#         print(0) # Impossible
+
+from typing import List, Tuple
 
 no_color, black, white = 0, 1, -1
 # colors = [no_color] * n
@@ -24,18 +32,33 @@ no_color, black, white = 0, 1, -1
 
 # See:
 # https://prd-xxx.hateblo.jp/entry/2017/10/13/004256
-def is_bipartite(start_id: int, graph: List[List], colors: List[int]):
+def is_bipartite(
+    start_id: int,
+    graph: List[List],
+    colors: List[int],
+    black_count: int = 0,
+    white_count: int = 0,
+) -> Tuple[bool, int, int]:
     stack = [(start_id, black)]  # vertex, color
 
     while stack:
         vertex, color = stack.pop()
+
+        if colors[vertex] != no_color:
+            continue
+
         colors[vertex] = color
+
+        if color == black:
+            black_count += 1
+        elif color == white:
+            white_count += 1
 
         for to in graph[vertex]:
             if colors[to] == color:
-                return False
+                return False, 0, 0
 
             if colors[to] == no_color:
                 stack.append((to, -color))  # Invert colors.
 
-    return True
+    return True, black_count, white_count
