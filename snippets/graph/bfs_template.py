c4f68ec45a6454686cb5e581ee06d4916682329c
Kato Hiroki <k.hiro1818@gmail.com>
2023-08-22 07:32:57 +0900

    ✨ Add BFS template for graph (#338)

--- snippets/graph/bfs_template.py
+++ snippets/graph/bfs_template.py
@@ -54,3 +54,52 @@ def bfs_for_grid(
 visited, dist = bfs_for_grid(grid=grid, h=h, w=w, sy=sy, sx=sx)
 
 # ===
+
+
+# Graph ===
+
+from typing import List, Tuple
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+
+for _ in range(m):
+    ai, bi = map(int, input().split())
+    ai -= 1
+    bi -= 1
+
+    graph[ai].append(bi)
+    # graph[bi].append(ai) # non-directed
+
+inf = 10**18
+
+
+def bfs_for_graph(
+    vertex_count: int, graph: List[List[int]], start_id: int
+) -> Tuple[List[bool], List[int]]:
+    d = deque([start_id])
+    visited = [False] * vertex_count
+    dist = [inf] * vertex_count
+    dist[start_id] = 0
+
+    while d:
+        cur = d.pop()
+
+        if visited[cur]:
+            continue
+
+        visited[cur] = True
+
+        for to in graph[cur]:
+            if visited[to]:
+                continue
+
+            d.append(to)
+            dist[to] = min(dist[to], dist[cur] + 1)
+
+    return visited, dist
+
+
+start_id = 0
+visited, dist = bfs_for_graph(vertex_count=n, graph=graph, start_id=start_id)
+# ===
